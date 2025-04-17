2fc310b7422135cec6ba490e75a614d3db703d5e
shivam130502 <shivamdhir90@gmail.com>
2025-04-16 23:55:28 -0700

    creating UserRepository to handle verification using DB

--- src/main/java/com/shivam/controller/MyUserDetailsService.java
+++ src/main/java/com/shivam/controller/MyUserDetailsService.java
@@ -1,5 +1,22 @@
 package com.shivam.controller;
 
-public class MyUserDetailsService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername();
+
+        return null;
+
+    }
 
 }
